c6e60753d7a939882c71a696bef048722cd2f6de
GauravTripathi10 <tripathi789gaurav@gmail.com>
2023-02-20 18:01:23 +0530

    This is The TypeCasting of Java

---
+++ TypeCasting.java
@@ -0,0 +1,23 @@
+public class TypeCasting{
+    public static void main(String[] args){
+       float bs=12.3f;
+       System.out.println(bs);
+       long bm=12222222222222l;
+       System.out.println(bm);
+    //    
+    int a=12;
+    int b;
+    b=a;
+    System.out.println("The Value Of B is ="+b);
+    double num1=123.22;
+    int num2;
+    num2=(int)num1;
+    // This is the Type casting of the Value beacuse the type of the double cant store in integer
+    System.out.println(num2);
+        int n1=10;
+        int  n2=2;
+        double result=(n1/n2);
+        System.out.println(result);
+
+    }
+}
